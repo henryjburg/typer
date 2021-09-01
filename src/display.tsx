fdfaa9345b6cdde6c86cddd3f02413096f50513a
Henry Burgess <henryjburg@gmail.com>
2021-09-01 12:47:25 +1000

    Update input and styling

--- src/display.tsx
+++ src/display.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { render } from 'react-dom';
 
 import { Container } from 'semantic-ui-react';
 
+const LEAD = '>>  ';
+
+export const CommandInput = (props: any) => {
+  function handleInput(event: any) {
+    props.inputRegion.current.append("Hello!");
+  }
+
+  // Add 'keydown' event listener
+  document.addEventListener('keydown', handleInput);
+
+  return (
+    <div className="command-input">
+      {LEAD} <input autoFocus></input>
+    </div>
+  );
+}
+
 export function start() {
+  const commandInput = createRef();
+
   render(
     (
       <>
         <Container className="display-container">
           <Container className="display-inner crt">
-            <h1>hello world</h1>
+            <div ref={commandInput}></div>
+            <CommandInput inputRegion={commandInput} />
           </Container>
         </Container>
         <Container className="status-container">
           <Container className="status-inner">
             <h1>Status</h1>
-            <p>ON</p>
             <p>Bytes Free: 5938</p>
           </Container>
         </Container>
       </>
     ),
     document.getElementById('app')
-  )
+  );
 }
